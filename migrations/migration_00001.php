1997b6482343345c07f91b6ef559d83472d62e99
rafiq boubaker <rafik.towati@gmail.com>
2026-06-26 17:59:39 +0300

    some important improvement

--- migrations/migration_00001.php
+++ migrations/migration_00001.php
@@ -1,13 +1,11 @@
 <?php
-if(!class_exists("AfwSession")) die("Denied access");
+if (!class_exists("AfwSession")) die("Denied access");
+/**
+ * @var string $migration_error
+ */
 
 $server_db_prefix = AfwSession::currentDBPrefix();
-try
-{
-
-
-}
-catch(Exception $e)
-{
+try {
+} catch (Exception $e) {
     $migration_error .= " " . $e->getMessage();
-}    
+}
